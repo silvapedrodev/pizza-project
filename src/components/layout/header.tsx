b24b97fe89ffb3752c53d3487235f4ee79b916ce
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2025-12-19 15:51:13 -0300

    feat: add auth buttons and session check to header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link"
-import { Button } from "../ui/button"
 import { CartButton } from "../cart/cart-button"
+import { LoginAreaButton } from "../login-area/login-area-button"
+import { cookies } from "next/headers"
 
-export const Header = () => {
+export const Header = async () => {
+  const cookieStore = await cookies()
+  const token = cookieStore.get('token')
+
   return (
     <header className="container mx-auto flex my-4 p-5 items-center justify-between bg-secondary rounded-md">
       <Link href="/">
         <div className="text-2xl font-bold">Pizza</div>
       </Link>
       <div className="flex gap-2">
-        <Button>Login / cadastro</Button>
+        <LoginAreaButton initialState={token ? true : false}/>
         <CartButton />
       </div>
     </header>
