6d4e1ac05345192ca3ddf19d01c3793527175e47
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2025-12-19 16:24:56 -0300

    feat: add authentication dialog

---
+++ src/components/login-area/login-area-dialog.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { useAuth } from "@/stores/auth"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog"
+import { useState } from "react"
+import { Button } from "../ui/button"
+import { ArrowLeft } from "lucide-react"
+
+type Steps = "EMAIL" | "SIGNUP" | "SIGNIN"
+
+export const LoginAreaDialog = () => {
+  const auth = useAuth()
+
+  const [step, setStep] = useState<Steps>("EMAIL")
+
+  return (
+    <Dialog
+      open={auth.open}
+      onOpenChange={open => auth.setOpen(open)}
+    >
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="flex items-center gap-2">
+            {step != 'EMAIL' &&
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={() => setStep('EMAIL')}
+              >
+                <ArrowLeft className="size-4" />
+              </Button>
+            }
+
+            {step === "EMAIL" && "Login / Cadastro"}
+            {step === "SIGNUP" && "Cadastro"}
+            {step === "SIGNIN" && "Login"}
+          </DialogTitle>
+        </DialogHeader>
+
+        <div className="flex flex-col gap-4">
+          {step === 'EMAIL' &&
+            <div>Email</div>
+          }
+          {step === 'SIGNIN' &&
+            <div>Login</div>
+          }
+          {step === 'SIGNUP' &&
+            <div>Cadastro</div>
+          }
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
