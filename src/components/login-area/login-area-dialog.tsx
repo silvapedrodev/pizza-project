4821d7cbe6bd9663db7d9621e52358954e961f30
Pedro Silva <150725974+silvapedrodev@users.noreply.github.com>
2025-12-20 12:44:53 -0300

    fix: sync auth store with token cookie on load

--- src/components/login-area/login-area-dialog.tsx
+++ src/components/login-area/login-area-dialog.tsx
@@ -2,11 +2,12 @@
 
 import { useAuth } from "@/stores/auth"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button } from "../ui/button"
 import { ArrowLeft } from "lucide-react"
 import { LoginAreaStepEmail } from "./login-area-step-email"
 import { LoginAreaSignup } from "./login-area-step-signup"
+import { getCookie } from "cookies-next/client"
 
 type Steps = "EMAIL" | "SIGNUP" | "SIGNIN"
 
@@ -15,6 +16,11 @@ export const LoginAreaDialog = () => {
 
   const [step, setStep] = useState<Steps>("EMAIL")
   const [emailField, setEmailField] = useState('')
+
+  useEffect(() => {
+    const token = getCookie('token')
+    if(token) auth.setToken(token)
+  }, [])
 
   const handleStepEmail = (hasEmail: boolean, email: string) => {
     setEmailField(email)
